a8f25dae41bc92c9aa92ef419c5ca39779916398
leonardpaulus <leonard.paulus@agenturignis.de>
2021-10-29 11:05:24 +0200

    :sparkles: Add get all cats and post a cat

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,9 @@ if (!process.env.MONGODB_URI) {
   throw new Error('No MongoDB URL dotenv variable');
 }
 
+const app = express();
+const port = 3000;
+
 // Custom middleware to log requests
 app.use((request, _response, next) => {
   console.log('Request received', request.url);
@@ -18,8 +21,35 @@ app.use((request, _response, next) => {
 // Middleware for parsing application/json
 app.use(express.json());
 
-const app = express();
-const port = 3000;
+// Post a cat to the Database
+app.post('/api/cats', async (request, response) => {
+  const catsCollection = getCatsCollection();
+  const newCat = request.body;
+  const isCatKnown = await catsCollection.findOne({ name: newCat.name });
+
+  if (
+    typeof newCat.name !== 'string' ||
+    typeof newCat.age !== 'number' ||
+    typeof newCat.race !== 'string'
+  ) {
+    response.status(400).send('Missing properties');
+    return;
+  }
+
+  if (isCatKnown) {
+    response.status(409).send('Cat already lives in the collection');
+  } else {
+    catsCollection.insertOne(newCat);
+    response.status(200).send('Cat has been added to the collection');
+  }
+});
+
+app.get('/api/cats', async (_request, response) => {
+  const catsCollection = getCatsCollection();
+  const catCursor = catsCollection.find();
+  const allCats = await catCursor.toArray();
+  response.status(200).send(allCats);
+});
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
